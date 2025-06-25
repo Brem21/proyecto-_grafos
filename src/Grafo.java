ad76dfd4fac1ef6571e4addcbc45f7612f69a605
Brem21 <bremsuarez111@hotmail.com>
2025-06-25 13:50:50 -0500

    agregado metodo dirigido que abarque del 1 al 20

---
+++ src/Grafo.java
@@ -0,0 +1,38 @@
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+class Grafo {
+    Map<String, Nodo> nodos = new HashMap<>();
+    Map<Nodo, List<Arco>> adyacencia = new HashMap<>();
+
+    public void agregarNodo(Nodo nodo) {
+        nodos.put(nodo.id, nodo);
+        adyacencia.put(nodo, new ArrayList<>());
+    }
+
+    public void agregarArco(String origenId, String destinoId, double peso) {
+        Nodo origen = nodos.get(origenId);
+        Nodo destino = nodos.get(destinoId);
+        if (origen != null && destino != null) {
+            adyacencia.get(origen).add(new Arco(origen, destino, peso));
+        }
+    }
+
+    public List<Nodo> rutaOptima(String src, String dst, Ataque tipo) {
+        return RutaOptima.dijkstra(this, src, dst);
+    }
+
+    // Método para inicializar nodos del 1 al 20
+    public void inicializarNodos1a20() {
+        for (int i = 1; i <= 20; i++) {
+            String id = String.valueOf(i);
+            if (!nodos.containsKey(id)) {
+                Nodo nodo = new Nodo(id, Nodo.Tipo.ROUTER, new Point(0, 0));
+                agregarNodo(nodo);
+            }
+        }
+    }
+
+}
